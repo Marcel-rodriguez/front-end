4abff0dab8eb621d234b2043344f2153660441fe
John Merz <jmerz826@gmail.com>
2022-02-02 23:42:10 -0500

    reset axiosWithAuth

--- src/authentication/axiosWithAuth.js
+++ src/authentication/axiosWithAuth.js
@@ -3,16 +3,6 @@ import axios from "axios";
 const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
 
-  axios
-    .post(
-      "https://secret-family-recipes-8.herokuapp.com/api/auth/validateToken",
-      token
-    )
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((err) => console.error(err));
-
   return axios.create({
     headers: {
       authorization: token,
