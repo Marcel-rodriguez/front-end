1c44a55b2cce50e625988bd3474074b576c28515
John Merz <jmerz826@gmail.com>
2022-02-02 22:09:21 -0500

    posting token to auth endpoint

--- src/authentication/axiosWithAuth.js
+++ src/authentication/axiosWithAuth.js
@@ -1,14 +1,24 @@
 import axios from "axios";
 
 const axiosWithAuth = () => {
-    const token = localStorage.getItem("token");
+  const token = localStorage.getItem("token");
 
-    return axios.create({
-        headers: {
-            authorization: token
-        },
-        baseURL: "https://secret-family-recipes-8.herokuapp.com/"
+  axios
+    .post(
+      "https://secret-family-recipes-8.herokuapp.com/api/auth/validateToken",
+      token
+    )
+    .then((res) => {
+      console.log(res);
     })
-}
+    .catch((err) => console.error(err));
+
+  return axios.create({
+    headers: {
+      authorization: token,
+    },
+    baseURL: "https://secret-family-recipes-8.herokuapp.com/",
+  });
+};
 
 export default axiosWithAuth;
