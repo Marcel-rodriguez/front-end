65750e354b4170482b8605d7a13183635f6bb713
John Merz <jmerz826@gmail.com>
2022-02-03 00:09:51 -0500

    removes token if not valid

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,16 +1,33 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Route, Redirect } from "react-router-dom";
+import axios from "axios";
 
 const PrivateRoute = (props) => {
-    const {component:Component, ...rest} = props;
-    
-    return <Route {...rest} render={()=> {
-        if(localStorage.getItem('token')) {
-            return <Component/>
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    axios
+      .post(
+        "https://secret-family-recipes-8.herokuapp.com/api/auth/validateToken",
+        { token }
+      )
+      .catch(() => {
+        localStorage.removeItem("token");
+      });
+  }, []);
+  const { component: Component, ...rest } = props;
+
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        if (localStorage.getItem("token")) {
+          return <Component />;
         } else {
-            return <Redirect to="/"/>
-        }       
-    }}/>
-}
+          return <Redirect to="/" />;
+        }
+      }}
+    />
+  );
+};
 
 export default PrivateRoute;
